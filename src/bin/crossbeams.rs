17cce8263fea33b90d8f6df4feca91d36e5e7b19
Hongxu <hongxuchen1989@gmail.com>
2017-07-11 15:32:27 +0800

    threadpool

--- src/bin/crossbeams.rs
+++ src/bin/crossbeams.rs
@@ -9,71 +9,73 @@ use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 use std::collections::VecDeque;
-use rand::{thread_rng, Rng};
+use rand::{Rng, thread_rng};
 
 struct MutMgr {}
 
 impl MutMgr {
     fn run(sender: mpsc::Sender<i32>, receiver: mpsc::Receiver<i32>) {
         let mut count = 0;
-        let mut_pool = ThreadPool::new(1);
-        let send_pool = ThreadPool::new(1);
+        let mut_pool = ThreadPool::new(10);
+        let send_pool = ThreadPool::new(10);
         // let mut mut_pool = BurstPool::new();
         // let mut send_pool = BurstPool::new();
 
         let (tx, rx): (mpsc::Sender<i32>, mpsc::Receiver<i32>) = channel();
 
         // receive
-        thread::spawn(move ||
-            loop {
-                // match receiver.try_iter().next() {
-                //     Some(item) => {
-                //         let mut count = 0;
-                //         for entry in receiver.iter() {
-                //             let tx = tx.clone();
-                //             mut_pool.execute(move || {
-                //                 thread::sleep(Duration::new(0, 100000000));
-                //                 println!("MutMgr received: {}", entry);
-                //                 let mutated = entry + 1;
-                //                 tx.send(mutated).unwrap();
-                //             });
-                //             count += 1;
-                //         }
-                //         println!("MutMgr received {} entries", count);
-                //     },
-                //     None => {}
-                // }
-                if let Ok(item) = receiver.try_recv() {
-                    let tx = tx.clone();
-                    mut_pool.execute(move || {
-                        println!("MutMgr received: {}, {}/{}", item, mut_pool.active_count(), mut_pool.max_count());
-                        // thread::sleep(Duration::new(0, 300_000_000));
-                        thread::sleep(Duration::new(1, 0));
-                        let mutated = item + 1;
-                        tx.send(mutated).unwrap();
-                    })
-                    // mut_pool.spawn(move |item| {
-                    //     println!("MutMgr received: {}", item);
-                    //     thread::sleep(Duration::new(0, 100000000));
-                    //     let mutated = item + 1;
-                    //     tx.send(mutated).unwrap();
-                    // });
-                    // mut_pool.send(item);
-                }
-                
-        });
+        thread::spawn(move || loop {
+            // match receiver.try_iter().next() {
+            //     Some(item) => {
+            //         let mut count = 0;
+            //         for entry in receiver.iter() {
+            //             let tx = tx.clone();
+            //             mut_pool.execute(move || {
+            //                 thread::sleep(Duration::new(0, 100000000));
+            //                 println!("MutMgr received: {}", entry);
+            //                 let mutated = entry + 1;
+            //                 tx.send(mutated).unwrap();
+            //             });
+            //             count += 1;
+            //         }
+            //         println!("MutMgr received {} entries", count);
+            //     },
+            //     None => {}
+            // }
+            println!(
+                "mut pool: {}/{}",
+                mut_pool.active_count(),
+                mut_pool.max_count()
+            );
+            if let Ok(item) = receiver.try_recv() {
+                let tx = tx.clone();
+                mut_pool.execute(move || {
+                    println!("MutMgr received: {}", item);
+                    // thread::sleep(Duration::new(0, 300_000_000));
+                    thread::sleep(Duration::new(1, 0));
+                    let mutated = item + 1;
+                    tx.send(mutated).unwrap();
+                })
+                // mut_pool.spawn(move |item| {
+                //     println!("MutMgr received: {}", item);
+                //     thread::sleep(Duration::new(0, 100000000));
+                //     let mutated = item + 1;
+                //     tx.send(mutated).unwrap();
+                // });
+                // mut_pool.send(item);
+            }
 
-        
+        });
 
         // send
         loop {
             if let Ok(item) = rx.try_recv() {
                 let sender = sender.clone();
                 send_pool.execute(move || {
-                    println!("MutMgr sending: {} {}/{}", item, mut_pool.active_count(), mut_pool.max_count());
+                    println!("MutMgr sending: {}", item);
                     sender.send(item).unwrap();
                 });
-                    count += 1;
+                count += 1;
                 // send_pool.spawn(move |item| {
                 //     println!("MutMgr sending: {}", item);
                 //     sender.send(item).unwrap();
@@ -91,27 +93,31 @@ struct RunMgr {}
 
 impl RunMgr {
     fn run(sender: mpsc::Sender<i32>, receiver: mpsc::Receiver<i32>) {
-        let mut_pool = ThreadPool::new(3);
-        let send_pool = ThreadPool::new(3);
+        let mut_pool = ThreadPool::new(30);
+        let send_pool = ThreadPool::new(30);
         let (tx, rx): (mpsc::Sender<i32>, mpsc::Receiver<i32>) = channel();
         let mut count = 0;
         // receive
-        thread::spawn(move ||
-            {
-                        
-                loop {
+        thread::spawn(move || {
+
+            loop {
+                println!(
+                    "run pool: {}/{}",
+                    mut_pool.active_count(),
+                    mut_pool.max_count()
+                );
                 match receiver.try_recv() {
                     Ok(item) => {
                         let tx = tx.clone();
                         mut_pool.execute(move || {
                             // let mut rng = rand::thread_rng();
-                            println!("RunMgr received: {} {}/{}", item, mut_pool.active_count(), mut_pool.max_count());
+                            println!("RunMgr received: {}", item);
                             // thread::sleep(Duration::new(0, rng.gen_range::<u32>(300000000u32, 600000000u32)));
                             thread::sleep(Duration::new(3, 0));
                             let mutated = item + 1;
                             tx.send(mutated).unwrap();
                         });
-                    },
+                    }
                     _ => {}
                 }
             }
@@ -127,7 +133,7 @@ impl RunMgr {
                         sender.send(item).unwrap();
                     });
                     count += 1;
-                },
+                }
                 _ => {}
             }
             if count >= 100 {
@@ -142,25 +148,29 @@ struct TraceMgr {}
 
 impl TraceMgr {
     fn run(sender: mpsc::Sender<i32>, receiver: mpsc::Receiver<i32>) {
-        let mut_pool = ThreadPool::new(1);
-        let send_pool = ThreadPool::new(1);
+        let mut_pool = ThreadPool::new(10);
+        let send_pool = ThreadPool::new(10);
         let (tx, rx): (mpsc::Sender<i32>, mpsc::Receiver<i32>) = channel();
         let mut count = 0;
         // receive
-        thread::spawn(move ||
-            loop {
-                match receiver.try_recv() {
-                    Ok(item) => {
-                        let tx = tx.clone();
-                        mut_pool.execute(move || {
-                            println!("TraceMgr received: {}", item);
-                            thread::sleep(Duration::new(1, 0));
-                            let mutated = item + 1;
-                            tx.send(mutated).unwrap();
-                        });
-                    },
-                    _ => {}
+        thread::spawn(move || loop {
+            println!(
+                "trace pool: {}/{}",
+                mut_pool.active_count(),
+                mut_pool.max_count()
+            );
+            match receiver.try_recv() {
+                Ok(item) => {
+                    let tx = tx.clone();
+                    mut_pool.execute(move || {
+                        println!("TraceMgr received: {}", item);
+                        thread::sleep(Duration::new(1, 0));
+                        let mutated = item + 1;
+                        tx.send(mutated).unwrap();
+                    });
                 }
+                _ => {}
+            }
         });
 
         // send
@@ -173,7 +183,7 @@ impl TraceMgr {
                         sender.send(item).unwrap();
                     });
                     count += 1;
-                },
+                }
                 _ => {}
             }
             if count >= 100 {
@@ -204,7 +214,9 @@ fn main() {
 
     let mutr = thread::spawn(move || { MutMgr::run(to_runner_sender, mutator_receiver); });
     let run = thread::spawn(move || { RunMgr::run(to_tracer_sender, runner_receiver); });
-    let tra = thread::spawn(move || { TraceMgr::run(to_mutator_sender, tracer_receiver); });
+    let tra = thread::spawn(move || {
+        TraceMgr::run(to_mutator_sender, tracer_receiver);
+    });
 
 
     mutr.join();
