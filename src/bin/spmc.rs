db5a260c69d45be3f7ebd309f45360a7090916ef
Hongxu <hongxuchen1989@gmail.com>
2017-07-12 14:12:34 +0800

    spmc

---
+++ src/bin/spmc.rs
@@ -0,0 +1,27 @@
+extern crate spmc;
+
+use std::thread;
+use std::thread::JoinHandle;
+
+fn main() {
+
+
+    let (tx, rx) = spmc::channel();
+
+    let mut handles: Vec<JoinHandle<()>> = Vec::new();
+    for n in 0..5 {
+        let rx = rx.clone();
+        handles.push(thread::spawn(move || {
+            let msg = rx.recv().unwrap();
+            println!("worker {} recvd: {}", n, msg);
+        }));
+    }
+
+    for i in 0..5 {
+        tx.send(i * 2).unwrap();
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
