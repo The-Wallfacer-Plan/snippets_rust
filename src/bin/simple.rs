7c6f58efcf2baf37c1f4ee181caec302f0588c54
Hongxu <hongxuchen1989@gmail.com>
2017-10-14 22:35:05 +0800

    corpus

--- src/bin/simple.rs
+++ src/bin/simple.rs
@@ -1,41 +1,12 @@
-use std::collections::VecDeque;
-
-fn v1() {
-    let mut v = vec![1, 2, 3];
-    let mut h = v[0];
-    // let h = v.first().unwrap();
-    v.push(4);
-    v.push(5);
-    h = 0;
-    println!("{}, {:?}", h, v);
-}
-
-fn v2() {
-    let mut v = VecDeque::new();
-    for i in 1..4 {
-        v.push_back(i);
-    }
-    let mut h = v[0];
-    // let h = v.front().unwrap();
-    v.push_back(4);
-    v.push_back(5);
-    h = 0;
-    println!("{}, {:?}", h, v);
-}
-
-fn test(v: &mut Vec<usize>) -> usize {
-    let e = v.pop().unwrap();
-    e + v.len()
-}
-
-struct Stat {
-    i: u8,
-    s: String,
-}
-
 fn main() {
-    let mut v = vec![1, 2, 3];
-    let res = test(&mut v);
-    let l = v.len();
-    println!("{}, {}", res, l);
+    let arr = [1u8, 2, 3, 4];
+    {
+        let pos: &u8 = unsafe { arr.get_unchecked(2) };
+        let v = pos + 1u8;
+        println!("v={}, pos={}, *pos={}", v, pos, *pos);
+    }
+    {
+        let pos: &mut u8 = unsafe { arr.get_unchecked_mut(2) };
+        // let v = pos + 1u8;
+    }
 }
