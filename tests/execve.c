c093959070b046c6200343bf87374cd1b5c629cb
hongxuchen <hongxu_chen@foxmail.com>
2025-02-05 17:31:28 +0800

    chore: remove tests/ folder

--- tests/execve.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int main(void) {
-    char* bin = "/usr/bin/printenv";
-    char*argv[] = {bin, "MY_VAR", NULL};
-    char*envp[] = {"MY_VAR=lol", NULL};
-    execve(bin, argv, envp);
-    return 0;
-}
--- tests/hello.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char  ** argv) {
-    printf("argc=%d\n", argc);
-    printf("argv0 is %s\n", argv[0]);
-    printf("argv1 is *%s*\n", argv[1]);
-    printf("hello world!\n");
-}
